3c2329c5d2f4b725b924644e536a311b53c78863
nnm_t <noname@kamisawa.net>
2017-09-07 23:07:52 +0900

    TrainTripThinker Project初期設定

--- TrainTripThinker/Properties/AssemblyInfo.cs
+++ TrainTripThinker/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("TrainTripThinker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Train Trip Thinker")]
+[assembly: AssemblyDescription("鉄道旅行特化行程表作成アプリケーション")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TrainTripThinker")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("HongNam Yuu Thee Nai")]
+[assembly: AssemblyProduct("Train Trip Thinker")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Noname Kamisawa")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
--- TrainTripThinker/View/MainWindow.xaml.cs
+++ TrainTripThinker/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TrainTripThinker
+namespace TrainTripThinker.View
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
