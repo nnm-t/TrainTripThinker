ba71043154fb5e91fb72bfd1dd1b16cce57c0d06
nnm_t <noname@kamisawa.net>
2017-09-10 14:26:01 +0900

    ドキュメントクラス、ロジックメインクラス作成

---
+++ TrainTripThinker.Core/Data/Itinerary.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TrainTripThinker.Core.Data
+{
+    /// <summary>
+    ///     行程表
+    /// </summary>
+    public class Itinerary
+    {
+        /// <summary>
+        ///     コンストラクタ
+        /// </summary>
+        public Itinerary()
+        {
+            this.Elements = new ObservableCollection<ItineraryElement>();
+        }
+
+        /// <summary>
+        ///     行程表のアイテムのコレクション
+        /// </summary>
+        public ObservableCollection<ItineraryElement> Elements { get; set; }
+    }
+}
---
+++ TrainTripThinker.Core/Data/TttDocument.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace TrainTripThinker.Core.Data
+{
+    /// <summary>
+    /// Train Trip Thinkerのドキュメントクラス
+    /// </summary>
+    public class TttDocument
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public TttDocument()
+        {
+            this.Itineraries = new ObservableCollection<Itinerary>();
+        }
+
+        /// <summary>
+        /// 行程表のコレクション
+        /// </summary>
+        public ObservableCollection<Itinerary> Itineraries { get; set; }
+    }
+}
