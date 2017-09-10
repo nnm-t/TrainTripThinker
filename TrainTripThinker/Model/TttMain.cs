6a2bd0716c330984810944da15a6bc4615039d79
nnm_t <noname@kamisawa.net>
2017-09-10 14:42:10 +0900

    アプリケーション側Mainクラス作成

---
+++ TrainTripThinker/Model/TttMain.cs
@@ -0,0 +1,23 @@
+﻿using TrainTripThinker.Core;
+
+namespace TrainTripThinker.Model
+{
+    /// <summary>
+    ///     Train Trip Thinkerメインクラス
+    /// </summary>
+    public class TttMain
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public TttMain()
+        {
+            this.Main = new Main();
+        }
+
+        /// <summary>
+        /// ロジッククラスのインスタンス
+        /// </summary>
+        public Main Main { get; }
+    }
+}
