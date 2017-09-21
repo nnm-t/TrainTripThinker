1a49f7efddcd47aed564d0bb38fa309da7c2e7b8
nnm_t <noname@kamisawa.net>
2017-09-22 00:58:46 +0900

    TransportClass 種別色追加、Color32→Color変換拡張メソッド作成

--- TrainTripThinker.Core/Data/Elements/TransportClass.cs
+++ TrainTripThinker.Core/Data/Elements/TransportClass.cs
@@ -1,5 +1,7 @@
 ﻿using Prism.Mvvm;
 
+using TrainTripThinker.Core.Structs;
+
 namespace TrainTripThinker.Core.Data
 {
     /// <summary>
@@ -8,10 +10,30 @@ namespace TrainTripThinker.Core.Data
     public class TransportClass : BindableBase
     {
         private string text;
+        private Color32 background;
+        private Color32 foreground;
 
-        public TransportClass(string text)
+        /// <inheritdoc />
+        /// <summary>
+        /// 種別インスタンス生成(白地黒文字)
+        /// </summary>
+        /// <param name="text">種別文字</param>
+        public TransportClass(string text) : this(text, Color32.White, Color32.Black)
+        {
+            
+        }
+
+        /// <summary>
+        /// 種別インスタンス生成(背景・文字色指定)
+        /// </summary>
+        /// <param name="text">種別文字</param>
+        /// <param name="background">背景色</param>
+        /// <param name="foreground">文字色</param>
+        public TransportClass(string text, Color32 background, Color32 foreground)
         {
             Text = text;
+            Background = background;
+            Foreground = foreground;
         }
 
         /// <summary>
@@ -20,19 +42,24 @@ namespace TrainTripThinker.Core.Data
         public static TransportClass Local => new TransportClass("普通");
 
         /// <summary>
+        /// 普通(黒地白文字)
+        /// </summary>
+        public static TransportClass LocalBlack => new TransportClass("普通", Color32.Black, Color32.White);
+
+        /// <summary>
         /// 快速
         /// </summary>
         public static TransportClass Rapid => new TransportClass("快速");
 
         /// <summary>
-        /// 急行
+        /// 急行(赤字白文字)
         /// </summary>
-        public static TransportClass Express => new TransportClass("急行");
+        public static TransportClass Express => new TransportClass("急行", Color32.Red, Color32.White);
 
         /// <summary>
-        /// 特急
+        /// 特急(赤字白文字)
         /// </summary>
-        public static TransportClass LimitedExpress => new TransportClass("特急");
+        public static TransportClass LimitedExpress => new TransportClass("特急", Color32.Red, Color32.White);
 
         /// <summary>
         /// 種別の文字列
@@ -43,5 +70,22 @@ namespace TrainTripThinker.Core.Data
             set => SetProperty(ref text, value);
         }
 
+        /// <summary>
+        /// 種別色(背景)
+        /// </summary>
+        public Color32 Background
+        {
+            get => background;
+            set => SetProperty(ref background, value);
+        }
+
+        /// <summary>
+        /// 種別色(前景)
+        /// </summary>
+        public Color32 Foreground
+        {
+            get => foreground;
+            set => SetProperty(ref foreground, value);
+        }
     }
 }
---
+++ TrainTripThinker/Extensions/Color32Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Media;
+
+using TrainTripThinker.Core.Structs;
+
+namespace TrainTripThinker.Extensions
+{
+    /// <summary>
+    /// <see cref="Color32"/>型の拡張メソッド群
+    /// </summary>
+    public static class Color32Extensions
+    {
+        /// <summary>
+        /// <see cref="Color"/>型に変換
+        /// </summary>
+        /// <param name="color32"><see cref="Color32"/>型インスタンス</param>
+        /// <returns><see cref="Color"/>型インスタンス</returns>
+        public static Color ToColor(this Color32 color32)
+        {
+            return Color.FromArgb(color32.A, color32.R, color32.G, color32.B);
+        }
+
+        /// <summary>
+        /// ベタ塗りの<see cref="SolidColorBrush"/>型に変換
+        /// </summary>
+        /// <param name="color32"><see cref="Color32"/>型インスタンス</param>
+        /// <returns><see cref="SolidColorBrush"/>型インスタンス</returns>
+        public static SolidColorBrush ToSolidColorBrush(this Color32 color32)
+        {
+            return new SolidColorBrush(color32.ToColor());
+        }
+    }
+}
