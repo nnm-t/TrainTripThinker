c5ef8fb9c570f1ce3084669c21f51deab8402723
nnm_t <noname@kamisawa.net>
2017-11-08 23:31:03 +0900

    座席アイコンViewへ表示・・・ComboBox選択時項目消える不具合有、要修正

---
+++ TrainTripThinker/Model/SeatIconPair.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Windows;
+
+using TrainTripThinker.Core.Enums;
+
+namespace TrainTripThinker.Model
+{
+    /// <summary>
+    /// 座席とアイコンのペア
+    /// </summary>
+    public class SeatIconPair
+    {
+        /// <summary>
+        /// 座席種類
+        /// </summary>
+        public SeatType SeatType { get; set; }
+
+        /// <summary>
+        /// アイコンに相当する<see cref="FrameworkElement"/>
+        /// </summary>
+        public FrameworkElement Icon { get; set; }
+    }
+}
---
+++ TrainTripThinker/View/BindingConverter/SeatIconPairConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+using TrainTripThinker.Core.Enums;
+using TrainTripThinker.Model;
+
+namespace TrainTripThinker.View
+{
+    public class SeatIconPairConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var seatType = (SeatType)value;
+            var pairs = (Application.Current.Resources["SeatTypeIconArray"] as ArrayExtension).Items
+                .Cast<SeatIconPair>();
+
+            return pairs?.First(x => x.SeatType.Equals(seatType));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as SeatIconPair)?.SeatType;
+        }
+    }
+}
