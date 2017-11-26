02746aade2529f66023c3748a99c2ced0a942d4a
nnm_t <noname@kamisawa.net>
2017-11-26 17:27:56 +0900

    ItineraryElement ViewModel作成

---
+++ TrainTripThinker/ViewModel/ItineraryElementViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Mvvm;
+
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+using TrainTripThinker.Core.Data;
+using TrainTripThinker.Core.Enums;
+
+namespace TrainTripThinker.ViewModel
+{
+    public abstract class ItineraryElementViewModel : BindableBase
+    {
+        protected ItineraryElementViewModel(ItineraryElement model)
+        {
+            Color = model.ObserveProperty(m => m.Color).ToReactiveProperty();
+            FreeForm = model.ObserveProperty(m => m.FreeForm).ToReactiveProperty();
+            Icon = model.ObserveProperty(m => m.Icon).ToReactiveProperty();
+        }
+
+        public ReactiveProperty<Color32> Color { get; }
+
+        public ReactiveProperty<string> FreeForm { get; }
+
+        public ReactiveProperty<ItineraryIcon> Icon { get; }
+    }
+}
