50de63c74bcf8c1bb6d21da14c57443fba032731
nnm_t <noname@kamisawa.net>
2017-12-18 23:38:04 +0900

    PrinterSelector実装

---
+++ TrainTripThinker/Model/Printing/IPrinter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Windows;
+
+namespace TrainTripThinker.Model.Printing
+{
+    public interface IPrinter
+    {
+        string Name { get; }
+
+        void Print(IEnumerable pages, Size pageSize);
+    }
+}
