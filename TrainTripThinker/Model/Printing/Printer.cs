4bda234a8ebf7d6db78c6aed0638b0f5c96ac57f
nnm_t <noname@kamisawa.net>
2017-12-19 23:28:05 +0900

    Printerクラス作成

---
+++ TrainTripThinker/Model/Printing/Printer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Printing;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Xps;
+
+using DotNetKit.Windows.Documents;
+
+namespace TrainTripThinker.Model.Printing
+{
+    public class Printer : IPrinter
+    {
+        private readonly PrintQueue printQueue;
+
+        public Printer(PrintQueue printQueue)
+        {
+            this.printQueue = printQueue;
+        }
+
+        public string Name => printQueue.Name;
+
+        public void Print(IEnumerable pages, Size pageSize)
+        {
+            bool isLandScape = pageSize.Width > pageSize.Height;
+            Size paperSize = isLandScape ? pageSize.Rotate90() : pageSize;
+
+            PrintTicket ticket = printQueue.DefaultPrintTicket;
+            ticket.PageMediaSize = paperSize.ToPageMediaSize();
+            ticket.PageOrientation = PageOrientation.Portrait;
+
+            // FixedDocumentを生成
+            FixedDocument document = new FixedDocumentCreator().FromDataContexts(pages, pageSize);
+
+            // 印刷
+            XpsDocumentWriter writer = PrintQueue.CreateXpsDocumentWriter(printQueue);
+            writer.Write(document);
+        }
+    }
+}
---
+++ TrainTripThinker/Utility/SizeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Printing;
+using System.Windows;
+
+namespace TrainTripThinker
+{
+    public static class SizeExtensions
+    {
+        public static Size Rotate90(this Size size)
+        {
+            return new Size(size.Height, size.Width);
+        }
+
+        public static PageMediaSize ToPageMediaSize(this Size size)
+        {
+            return new PageMediaSize(size.Width, size.Height);
+        }
+    }
+}
