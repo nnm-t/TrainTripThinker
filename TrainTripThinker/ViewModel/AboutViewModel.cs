f4baf0368b83a981eb8979a19872d25cd6d7939d
nnm_t <noname@kamisawa.net>
2017-12-24 16:27:57 +0900

    バージョン情報ウィンドウ実装

---
+++ TrainTripThinker/ViewModel/AboutViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Reactive.Bindings;
+using TrainTripThinker.Core.Utility;
+using TrainTripThinker.Model;
+
+namespace TrainTripThinker.ViewModel
+{
+    public class AboutViewModel : ViewModelBase
+    {
+        private bool isShowDialog;
+
+        public AboutViewModel()
+        {
+            ShowWikiCommand = new ReactiveCommand();
+            ShowWikiCommand.Subscribe(TttHelp.AccessToWiki);
+
+            ShowAboutCommand = new ReactiveCommand();
+            ShowAboutCommand.Subscribe(() => IsShowDialog = true);
+
+            CloseAboutCommand = new ReactiveCommand();
+            CloseAboutCommand.Subscribe(() => IsShowDialog = false);
+        }
+
+        public bool IsShowDialog
+        {
+            get => isShowDialog;
+            set => SetProperty(ref isShowDialog, value);
+        }
+
+        public ReactiveCommand ShowWikiCommand { get; }
+
+        public ReactiveCommand ShowAboutCommand { get; }
+
+        public ReactiveCommand CloseAboutCommand { get; }
+
+        public string AssemblyVersion => AssemblyInformation.GetAssemblyVersion().ToString();
+
+        public string AssemblyTitle => AssemblyInformation.GetAssemblyTitle();
+
+        public string AssemblyCopyright => AssemblyInformation.GetAssemblyCopyright();
+    }
+}
