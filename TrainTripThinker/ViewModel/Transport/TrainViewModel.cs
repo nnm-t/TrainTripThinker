5904b5c85058a27764c9e56c8ecb0d43c1b9deab
Noname Kamisawa <noname@kamisawa.net>
2018-01-17 23:27:06 +0900

    Trainクラス ViewModel作成

---
+++ TrainTripThinker/ViewModel/Transport/TrainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using TrainTripThinker.Core.Data;
+using TrainTripThinker.Core.Enums;
+
+namespace TrainTripThinker.ViewModel
+{
+    public class TrainViewModel : TransportBaseViewModel
+    {
+        public TrainViewModel(Train model) : base(model)
+        {
+            Class = model.ObserveProperty(m => m.Class).ToReactiveProperty();
+            LineColor = model.ObserveProperty(m => m.LineColor).ToReactiveProperty();
+            Seat = model.ObserveProperty(m => m.Seat).ToReactiveProperty();
+            HasRestRoom = model.ObserveProperty(m => m.HasRestRoom).ToReactiveProperty();
+            MealType = model.ObserveProperty(m => m.MealType).ToReactiveProperty();
+        }
+
+        public ReactiveProperty<TransportClass> Class { get; }
+
+        public ReactiveProperty<Color32> LineColor { get; }
+
+        public ReactiveProperty<TransportSeat> Seat { get; }
+
+        public ReactiveProperty<bool> HasRestRoom { get; }
+
+        public ReactiveProperty<MealType> MealType { get; }
+    }
+}
---
+++ TrainTripThinker/ViewModel/Transport/TransportBaseViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using TrainTripThinker.Core.Data;
+
+namespace TrainTripThinker.ViewModel
+{
+    public abstract class TransportBaseViewModel
+    {
+        protected TransportBaseViewModel(TransportBase model)
+        {
+            TransportNumber = model.ObserveProperty(m => m.TransportNumber).ToReactiveProperty();
+            Name = model.ObserveProperty(m => m.Name).ToReactiveProperty();
+            Destination = model.ObserveProperty(m => m.Destination).ToReactiveProperty();
+
+            Routes = model.Routes.ToReadOnlyReactiveCollection();
+        }
+
+        public ReactiveProperty<TransportNumber> TransportNumber { get; }
+
+        public ReadOnlyReactiveCollection<string> Routes { get; }
+
+        public ReactiveProperty<string> Name { get; }
+
+        public ReactiveProperty<string> Destination { get; }
+    }
+}
