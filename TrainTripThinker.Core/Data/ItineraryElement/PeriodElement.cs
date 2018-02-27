f780347871c934350d9b9996bd8510d1bae9adc6
Noname Kamisawa <noname@kamisawa.net>
2018-02-27 23:42:22 +0900

    発時刻変更時、発時刻>着時刻の場合は着時刻を発時刻に合わせる処理をModelへ実装

--- TrainTripThinker.Core/Data/ItineraryElement/PeriodElement.cs
+++ TrainTripThinker.Core/Data/ItineraryElement/PeriodElement.cs
@@ -1,26 +1,42 @@
 ﻿using System;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+
+using Reactive.Bindings.Extensions;
 
 using TrainTripThinker.Core.Utility;
 
 namespace TrainTripThinker.Core.Data
 {
-    public class PeriodElement : ItineraryElement
+    public class PeriodElement : ItineraryElement, IDisposable
     {
+        private readonly CompositeDisposable disposables;
+
         private Period<Departure> period;
+
 
         public PeriodElement(ItineraryElementDelegates delegates, DateTime beginTime) : base(delegates)
         {
+            disposables = new CompositeDisposable();
             CreatePeriodInstance(beginTime);
         }
 
         public PeriodElement(ItineraryElementDelegates delegates) : base(delegates)
         {
+            disposables = new CompositeDisposable();
             CreatePeriodInstance();
         }
 
         public PeriodElement()
         {
+            disposables = new CompositeDisposable();
             CreatePeriodInstance();
+
+        }
+
+        ~PeriodElement()
+        {
+            Dispose();
         }
 
         /// <summary>
@@ -32,14 +48,40 @@ namespace TrainTripThinker.Core.Data
             set => SetProperty(ref period, value);
         }
 
+        public void Dispose()
+        {
+            disposables.Dispose();
+        }
+
+        private void SubscribeBeginDateTimeMismatch()
+        {
+            Period.Begin.ObserveProperty(p => p.Date).Subscribe(dt =>
+            {
+                if (dt > Period.End.DateTime)
+                {
+                    Period.End.Date = dt;
+                }
+            }).AddTo(disposables);
+
+            Period.Begin.ObserveProperty(p => p.Time).Subscribe(dt =>
+            {
+                if (dt > Period.End.DateTime)
+                {
+                    Period.End.Time = dt;
+                }
+            }).AddTo(disposables);
+        }
+
         private void CreatePeriodInstance()
         {
             Period = new Period<Departure>(new Departure(), new Departure());
+            SubscribeBeginDateTimeMismatch();
         }
 
         private void CreatePeriodInstance(DateTime begin)
         {
             Period = new Period<Departure>(new Departure(begin), new Departure(begin));
+            SubscribeBeginDateTimeMismatch();
         }
     }
 }
